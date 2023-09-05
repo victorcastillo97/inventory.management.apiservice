65c4a85f2072f399feb2c98bfca157330c39ee32
victorcastillo <victor.saul.ca@gmail.com>
2023-09-04 23:53:00 -0500

    config.env.requirements

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,4 @@
-DATABASE_URL = "mysql+pymysql://root:password@localhost:33060/storedb3"
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+#DATABASE_URL = "mysql+pymysql://root:password@localhost:33060/storedb3"
